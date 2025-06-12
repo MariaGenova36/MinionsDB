5272974f4f3beb1269172cdb9ae5bab1f637f3f5
MariaGenova36 <mariq.genova@gmail.com>
2025-06-12 17:06:59 +0300

    Add files via upload

---
+++ MinionsDB/MinionsDB/DatabaseManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class DatabaseManager
+{
+    private readonly string connectionStringMaster = "Server=localhost;Database=master;Integrated Security=true;";
+    private readonly string connectionStringMinionsDB = "Server=localhost;Database=MinionsDB2;Integrated Security=true;";
+
+    public void CreateDatabase()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionStringMaster))
+        {
+            connection.Open();
+            string query = "IF DB_ID('MinionsDB2') IS NULL CREATE DATABASE MinionsDB2";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.ExecuteNonQuery();
+                Console.WriteLine("MinionsDB създадена успешно.");
+            }
+        }
+    }
+
+    public void CreateTables()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionStringMinionsDB))
+        {
+            connection.Open();
+
+            string[] tableQueries = new string[]
+            {
+                "CREATE TABLE Countries (Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(50))",
+                "CREATE TABLE Towns (Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(50), CountryCode INT FOREIGN KEY REFERENCES Countries(Id))",
+                "CREATE TABLE Minions (Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(50), Age INT, TownId INT FOREIGN KEY REFERENCES Towns(Id))",
+                "CREATE TABLE EvilnessFactors (Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(50))",
+                "CREATE TABLE Villains (Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(50), EvilnessFactorId INT FOREIGN KEY REFERENCES EvilnessFactors(Id))",
+                "CREATE TABLE MinionsVillains (MinionId INT FOREIGN KEY REFERENCES Minions(Id), VillainId INT FOREIGN KEY REFERENCES Villains(Id), PRIMARY KEY (MinionId, VillainId))"
+            };
+
+            foreach (var query in tableQueries)
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("Таблиците са създадени успешно.");
+        }
+    }
+
+    public void SeedBasicData()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionStringMinionsDB))
+        {
+            connection.Open();
+
+            string[] inserts = new string[]
+            {
+                // Countries
+                "INSERT INTO Countries (Name) VALUES ('Bulgaria'), ('USA'), ('Germany')",
+
+                // Towns
+                "INSERT INTO Towns (Name, CountryCode) VALUES ('Sofia', 1), ('Plovdiv', 1), ('New York', 2)",
+
+                // Evilness Factors
+                "INSERT INTO EvilnessFactors (Name) VALUES ('Super good'), ('Good'), ('Bad'), ('Evil'), ('Super evil')"
+            };
+
+            foreach (var query in inserts)
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("Базовите данни са въведени.");
+        }
+    }
+}
---
+++ MinionsDB/MinionsDB/DatabaseSeeder.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlClient;
+using System;
+
+public class DatabaseSeeder
+{
+    private readonly string connectionString = "Server=localhost;Database=MinionsDB2;Integrated Security=true;";
+
+    public void SeedData()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            var commands = new[]
+            {
+                // Злодеи
+                "INSERT INTO Villains (Name, EvilnessFactorId) VALUES ('Gru', 5)",
+                "INSERT INTO Villains (Name, EvilnessFactorId) VALUES ('Victor', 4)",
+                "INSERT INTO Villains (Name, EvilnessFactorId) VALUES ('Jilly', 3)",
+
+                // Миниони
+                "INSERT INTO Minions (Name, Age, TownId) VALUES ('Tom', 10, 1)",
+                "INSERT INTO Minions (Name, Age, TownId) VALUES ('Jerry', 12, 1)",
+                "INSERT INTO Minions (Name, Age, TownId) VALUES ('Mark', 11, 2)",
+                "INSERT INTO Minions (Name, Age, TownId) VALUES ('Bob', 9, 2)",
+                "INSERT INTO Minions (Name, Age, TownId) VALUES ('Ned', 13, 3)",
+                "INSERT INTO Minions (Name, Age, TownId) VALUES ('Jim', 8, 3)",
+
+                // Връзки
+                "INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (1,1),(2,1),(3,1),(4,1),(5,1),(6,1)",
+                "INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (1,2),(2,2),(3,2),(4,2)",
+                "INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (3,3),(4,3),(5,3),(6,3)"
+            };
+
+            foreach (var cmdText in commands)
+            {
+                using (SqlCommand cmd = new SqlCommand(cmdText, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("Данните са заредени.");
+        }
+    }
+}
